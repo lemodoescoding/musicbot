3d0a8499c90bf011c1dbf2ed95708077306c4d7b
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2026-06-19 11:09:48 +0700

    feat(embed): added embed
    - added basic implementation to send embed message
    - responds when user is typed 'embed' or used '/embed'
    - wrapped the embed generation in function to be callable with optional
      image URL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { Client, IntentsBitField } = require('discord.js')
+const { Client, IntentsBitField, EmbedBuilder } = require('discord.js')
 
 const client = new Client({
     intents: [
@@ -15,24 +15,45 @@ client.on('clientReady', (c) => {
     console.log(`[DISCORD BOT] ${c.user.tag} status is ready.`)
 })
 
+
+function createEmbed(image) {
+    const embed = new EmbedBuilder()
+        .setTitle("Embed title")
+        .setDescription('This is an embed description')
+        .setColor('Random')
+        .addFields({
+            name: 'Field title', value: 'Some random value', inline: true
+        }, {
+            name: '2nd Field title', value: 'Another Some random value', inline: true
+        }, {
+            name: '3rd Field title', value: 'Just Another Some random value', inline: true
+        })
+
+    if (image && typeof image === 'string') embed.setImage(image.trim())
+
+    return embed
+}
+
 client.on('interactionCreate', (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
     console.log(interaction.commandName)
 
-    // if (interaction.commandName === 'hey') {
-    //     interaction.reply('hey!')
-    // }
+    // if (interaction.commandName === 'add') {
+    //     const num1 = interaction.options.get('first-number').value;
+    //     const num2 = interaction.options.get('second-number').value;
     //
-    // if (interaction.commandName === 'ping') {
-    //     interaction.reply('Pong!')
+    //     interaction.reply(`The sum is ${num1 + num2}`)
     // }
 
-    if (interaction.commandName === 'add') {
-        const num1 = interaction.options.get('first-number').value;
-        const num2 = interaction.options.get('second-number').value;
+    if (interaction.commandName === 'embed') {
+        interaction.reply({ embeds: [createEmbed()] })
+    }
+})
 
-        interaction.reply(`The sum is ${num1 + num2}`)
+client.on('messageCreate', (message) => {
+    if (message.content === 'embed') {
+        message.channel.send({ embeds: [createEmbed('https://github.com/lemodoescoding/fp-mbd-kelompok-4-2026/raw/master/assets/FP_MBD_B_-_Kelompok_4.png')]})
     }
 })
 
--- src/register-commands.js
+++ src/register-commands.js
@@ -2,51 +2,47 @@ require('dotenv').config()
 
 const { REST, Routes, ApplicationCommandOptionType } = require('discord.js')
 
-// const commands = [
-//     {
-//         name: 'hey',
-//         description: 'Replies with hey!'
-//     },
-//     {
-//         name: 'ping',
-//         description: 'Pong!'
-//     }
-// ]
-
 const commands = [
     {
-        name: 'add',
-        description: 'Adds two numbers.',
-        options: [
-            {
-                name: 'first-number',
-                description: 'The first number.',
-                type: ApplicationCommandOptionType.Number,
-                required: true,
-                choices: [
-                    {
-                        name: 'one',
-                        value: 1,
-                    },
-                    {
-                        name: 'two',
-                        value: 2,
-                    },
-                    {
-                        name: 'three',
-                        value: 3,
-                    }
-                ]
-            },
-            {
-                name: 'second-number',
-                description: 'The second number.',
-                type: ApplicationCommandOptionType.Number,
-                required: true
-            }
-        ],
+        name: 'embed',
+        description: 'Sends an embed.'
     }
 ]
+
+// const commands = [
+//     {
+//         name: 'add',
+//         description: 'Adds two numbers.',
+//         options: [
+//             {
+//                 name: 'first-number',
+//                 description: 'The first number.',
+//                 type: ApplicationCommandOptionType.Number,
+//                 required: true,
+//                 choices: [
+//                     {
+//                         name: 'one',
+//                         value: 1,
+//                     },
+//                     {
+//                         name: 'two',
+//                         value: 2,
+//                     },
+//                     {
+//                         name: 'three',
+//                         value: 3,
+//                     }
+//                 ]
+//             },
+//             {
+//                 name: 'second-number',
+//                 description: 'The second number.',
+//                 type: ApplicationCommandOptionType.Number,
+//                 required: true
+//             }
+//         ],
+//     }
+// ]
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
